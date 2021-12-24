bf9fe95e667aa5c5833fea24f6500a01cadef0a9
Milo <zeroeclipse@live.com>
2021-12-23 21:07:53 -0800

    first commit

---
+++ challenges.js
@@ -0,0 +1,34 @@
+
+
+// There are  16 hourglasses in arr. An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum. The array will always be 6x6.
+
+// Print the largest (maximum) hourglass sum found in arr.
+
+let arr = [
+    [ 1, 1, 1, 0, 0, 0 ],
+    [ 0, 1, 0, 0, 0, 0 ],
+    [ 1, 1, 1, 0, 0, 0 ],
+    [ 0, 0, 2, 4, 4, 0 ],
+    [ 0, 0, 0, 2, 0, 0 ],
+    [ 0, 0, 1, 2, 4, 0 ]
+  ]
+
+
+const hourglassSum = (arr) => {
+    let largest = 0
+    for(let i = 0; i < arr.length - 2; i++ ){
+        for(let j = 0; j < arr.length - 2; j++){
+            let currentSum = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            if(i === 0 && j === 0){
+                largest = currentSum
+            }
+            if(currentSum > largest){
+                largest = currentSum
+            }
+        }
+}
+console.log(largest)
+    return largest
+}
+
+hourglassSum(arr)
